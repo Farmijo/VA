261aff8b0174b7407480f77a1930e52bd72dc79c
farmijo <farmijo16@gmail.com>
2016-10-04 20:45:23 +0200

    First commit

---
+++ Lab1/StudentVersion/src/Application.cpp
@@ -0,0 +1,110 @@
+#include "Application.h"
+#include "Utils.h"
+#include "Mesh.h"
+
+#include <cmath>
+
+//some globals
+Mesh* mesh = NULL;
+
+Application* Application::instance = NULL;
+
+Application::Application( SDL_Window* window )
+{
+	this->window = window;
+	instance = this;
+
+	// initialize attributes
+	// Warning: DO NOT CREATE STUFF HERE, USE THE INIT 
+	// things create here cannot access opengl
+	SDL_GetWindowSize( window, &window_width, &window_height );
+	std::cout << " * Window size: " << window_width << " x " << window_height << std::endl;
+
+	keystate = NULL;
+}
+
+//Here we have already GL working, so we can create meshes and textures
+void Application::init( void )
+{
+	std::cout << " * Path: " << getPath() << std::endl;
+
+	//OpenGL flags
+	glEnable( GL_CULL_FACE ); //render both sides of every triangle
+	glEnable( GL_DEPTH_TEST ); //check the occlusions using the Z buffer
+
+	//create our camera
+	camera = new Camera();
+	camera->lookAt( Vector3( 0, 25, 25 ), Vector3( 0, 0, 0 ), Vector3( 0, 1, 0 ) ); //position the camera and point to 0,0,0
+	camera->setPerspective( 70, window_width / (float)window_height, 0.1f, 10000 ); //set the projection, we want to be perspective
+
+	//create a plane mesh
+	mesh = new Mesh();
+	mesh->createPlane( 10 );
+
+	//hide the cursor
+	SDL_ShowCursor( true ); //hide or show the mouse
+}
+
+//what to do when the image has to be draw
+void Application::render( void )
+{
+	glClearColor( 1.0, 0.0, 0.0, 1.0 );
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+	//set the clear color (the background color)
+	glClearColor( 0.0, 0.0, 0.0, 1.0 );
+
+	// Clear the window and the depth buffer
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+	//Put the camera matrices on the stack of OpenGL (only for fixed rendering)
+	camera->set();
+
+	//Draw out world
+	drawGrid( 500 ); //background grid
+
+	mesh->render( GL_TRIANGLES );
+
+	glDisable( GL_BLEND );
+
+	//swap between front buffer and back buffer
+	SDL_GL_SwapWindow( this->window );
+}
+
+void Application::update( float seconds_elapsed )
+{
+	// EXERCISE PRACT 1
+
+	float speed = seconds_elapsed * 100; //the speed is defined by the seconds_elapsed so it goes constant
+	
+	if( mouse_state & SDL_BUTTON(SDL_BUTTON_LEFT) ) //is left button pressed?
+	{
+		// to complete
+	}
+	
+	if( keystate[ SDL_SCANCODE_LSHIFT ] ) speed *= 10; //move faster with left shift
+	// to complete for being able to move the camera
+}
+
+//Keyboard event handler (sync input)
+void Application::onKeyPressed( SDL_KeyboardEvent event )
+{
+	switch( event.keysym.sym )
+	{
+		case SDLK_ESCAPE: exit( 0 ); //ESC key, kill the app
+	}
+}
+
+
+void Application::onMouseButton( SDL_MouseButtonEvent event )
+{
+
+}
+
+void Application::setWindowSize( int width, int height )
+{
+	std::cout << "window resized: " << width << "," << height << std::endl;
+
+	// EXERCISE PRACT 1	
+}
+
---
+++ Lab1/StudentVersion/src/Mesh.cpp
@@ -0,0 +1,166 @@
+#include "Mesh.h"
+#include <cassert>
+#include "includes.h"
+
+Mesh::Mesh()
+{
+	vertices_vbo_id = 0;
+	normals_vbo_id = 0;
+	uvs_vbo_id = 0;
+	colors_vbo_id = 0;
+}
+
+Mesh::Mesh( const Mesh& m )
+{
+	vertices = m.vertices;
+	normals = m.normals;
+	uvs = m.uvs;
+	colors = m.colors;
+}
+
+Mesh::~Mesh()
+{
+	if( vertices_vbo_id ) glDeleteBuffersARB( 1, &vertices_vbo_id );
+	if( normals_vbo_id ) glDeleteBuffersARB( 1, &normals_vbo_id );
+	if( uvs_vbo_id ) glDeleteBuffersARB( 1, &uvs_vbo_id );
+	if( colors_vbo_id ) glDeleteBuffersARB( 1, &colors_vbo_id );
+}
+
+void Mesh::clear()
+{
+	vertices.clear();
+	normals.clear();
+	uvs.clear();
+	colors.clear();
+}
+
+
+void Mesh::render( int primitive )
+{
+	assert( vertices.size() && "No vertices in this mesh" );
+
+	glEnableClientState( GL_VERTEX_ARRAY );
+
+	if( vertices_vbo_id )
+	{
+		glBindBufferARB( GL_ARRAY_BUFFER_ARB, vertices_vbo_id );
+		glVertexPointer( 3, GL_FLOAT, 0, NULL );
+	}
+	else
+		glVertexPointer( 3, GL_FLOAT, 0, &vertices[ 0 ] );
+
+	if( normals.size() )
+	{
+		glEnableClientState( GL_NORMAL_ARRAY );
+		if( normals_vbo_id )
+		{
+			glBindBufferARB( GL_ARRAY_BUFFER_ARB, normals_vbo_id );
+			glNormalPointer( GL_FLOAT, 0, NULL );
+		}
+		else
+			glNormalPointer( GL_FLOAT, 0, &normals[ 0 ] );
+	}
+
+	if( uvs.size() )
+	{
+		glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+		if( uvs_vbo_id )
+		{
+			glBindBufferARB( GL_ARRAY_BUFFER_ARB, uvs_vbo_id );
+			glTexCoordPointer( 2, GL_FLOAT, 0, NULL );
+		}
+		else
+			glTexCoordPointer( 2, GL_FLOAT, 0, &uvs[ 0 ] );
+	}
+
+	if( colors.size() )
+	{
+		glEnableClientState( GL_COLOR_ARRAY );
+		if( colors_vbo_id )
+		{
+			glBindBufferARB( GL_ARRAY_BUFFER_ARB, colors_vbo_id );
+			glColorPointer( 4, GL_FLOAT, 0, NULL );
+		}
+		else
+			glColorPointer( 4, GL_FLOAT, 0, &colors[ 0 ] );
+	}
+
+	glDrawArrays( primitive, 0, (GLsizei)vertices.size() );
+	glDisableClientState( GL_VERTEX_ARRAY );
+
+	if( normals.size() )
+		glDisableClientState( GL_NORMAL_ARRAY );
+	if( uvs.size() )
+		glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+	if( colors.size() )
+		glDisableClientState( GL_COLOR_ARRAY );
+	glBindBufferARB( GL_ARRAY_BUFFER_ARB, 0 );
+}
+
+void Mesh::createQuad( float center_x, float center_y, float w, float h, bool flip_uvs )
+{
+	vertices.clear();
+	normals.clear();
+	uvs.clear();
+	colors.clear();
+
+	//create six vertices (3 for upperleft triangle and 3 for lowerright)
+
+	vertices.push_back( Vector3( center_x + w*0.5f, center_y + h*0.5f, 0.0f ) );
+	vertices.push_back( Vector3( center_x - w*0.5f, center_y - h*0.5f, 0.0f ) );
+	vertices.push_back( Vector3( center_x + w*0.5f, center_y - h*0.5f, 0.0f ) );
+	vertices.push_back( Vector3( center_x - w*0.5f, center_y + h*0.5f, 0.0f ) );
+	vertices.push_back( Vector3( center_x - w*0.5f, center_y - h*0.5f, 0.0f ) );
+	vertices.push_back( Vector3( center_x + w*0.5f, center_y + h*0.5f, 0.0f ) );
+
+	//texture coordinates
+	uvs.push_back( Vector2( 1.0f, flip_uvs ? 0.0f : 1.0f ) );
+	uvs.push_back( Vector2( 0.0f, flip_uvs ? 1.0f : 0.0f ) );
+	uvs.push_back( Vector2( 1.0f, flip_uvs ? 1.0f : 0.0f ) );
+	uvs.push_back( Vector2( 0.0f, flip_uvs ? 0.0f : 1.0f ) );
+	uvs.push_back( Vector2( 0.0f, flip_uvs ? 1.0f : 0.0f ) );
+	uvs.push_back( Vector2( 1.0f, flip_uvs ? 0.0f : 1.0f ) );
+
+	//all of them have the same normal
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+	normals.push_back( Vector3( 0.0f, 0.0f, 1.0f ) );
+}
+
+
+void Mesh::createPlane( float size )
+{
+	vertices.clear();
+	normals.clear();
+	uvs.clear();
+	colors.clear();
+
+	//create six vertices (3 for upperleft triangle and 3 for lowerright)
+
+	vertices.push_back( Vector3( size, 0, size ) );
+	vertices.push_back( Vector3( size, 0, -size ) );
+	vertices.push_back( Vector3( -size, 0, -size ) );
+	vertices.push_back( Vector3( -size, 0, size ) );
+	vertices.push_back( Vector3( size, 0, size ) );
+	vertices.push_back( Vector3( -size, 0, -size ) );
+
+	//all of them have the same normal
+	normals.push_back( Vector3( 0, 1, 0 ) );
+	normals.push_back( Vector3( 0, 1, 0 ) );
+	normals.push_back( Vector3( 0, 1, 0 ) );
+	normals.push_back( Vector3( 0, 1, 0 ) );
+	normals.push_back( Vector3( 0, 1, 0 ) );
+	normals.push_back( Vector3( 0, 1, 0 ) );
+
+	//texture coordinates
+	uvs.push_back( Vector2( 1, 1 ) );
+	uvs.push_back( Vector2( 1, 0 ) );
+	uvs.push_back( Vector2( 0, 0 ) );
+	uvs.push_back( Vector2( 0, 1 ) );
+	uvs.push_back( Vector2( 1, 1 ) );
+	uvs.push_back( Vector2( 0, 0 ) );
+}
+
